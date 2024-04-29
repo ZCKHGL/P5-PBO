4c875bfc7759b50e116c1fa123e6ef4cdd1487ff
A-6 <A-6@A-6>
2024-04-29 10:17:40 +0700

    new

---
+++ P5PBO/src/main/java/com/mycompany/p5pbo/Guradian.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.p5pbo;
+
+/**
+ *
+ * @author A-6
+ */
+public class Guradian {
+    public static void main(String[] args) {
+        System.out.println("Hawoo!");
+    }
+}
